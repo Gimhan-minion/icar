644953495686e4e8c6eab6cb1bf56fe6408b14b7
gajindubandara <gajindukb@gmail.com>
2022-11-20 10:27:15 +0530

    Fix login

---
+++ iCar2/app/src/main/java/com/uom/icar/Temp.java
@@ -0,0 +1,34 @@
+package com.uom.icar;
+
+
+public class Temp {
+    private static String NIC;
+    private static String jobID;
+    private static String viewUserID;
+
+
+    public static String getNIC() {
+        return NIC;
+    }
+
+    public static void setNIC(String NIC) {
+        Temp.NIC = NIC;
+    }
+
+    public static String getJobID() {
+        return jobID;
+    }
+
+    public static void setJobID(String jobID) {
+        Temp.jobID = jobID;
+    }
+
+    public static String getViewUserID() {
+        return viewUserID;
+    }
+
+    public static void setViewUserID(String viewUserID) {
+        Temp.viewUserID = viewUserID;
+    }
+
+}
