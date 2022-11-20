243e67a1c41ae5fc0b55e70eaa86a85c0941c6a5
gimhanminion <gimhan@entgra.io>
2022-11-20 10:07:09 +0530

    Fix ui fuel

--- iCar2/app/src/main/java/com/uom/icar/ui/manageService/EditServiceFragment.java
+++ iCar2/app/src/main/java/com/uom/icar/ui/manageService/EditServiceFragment.java
@@ -25,7 +25,7 @@ public class EditServiceFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit, container, false);
+        return inflater.inflate(R.layout.fragment_edit_service, container, false);
     }
 
     @Override
--- iCar2/app/src/main/java/com/uom/icar/ui/manageService/ViewService.java
+++ iCar2/app/src/main/java/com/uom/icar/ui/manageService/ViewService.java
@@ -25,7 +25,7 @@ public class ViewService extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view, container, false);
+        return inflater.inflate(R.layout.fragment_view_service, container, false);
     }
 
     @Override
