ceba5be2c219cfe6faac204f0f7a94a5040b6d5c
gajindubandara <gajindukb@gmail.com>
2022-11-23 13:56:42 +0530

    Fix navigation

--- iCar2/app/src/main/java/com/uom/icar/MainActivity.java
+++ iCar2/app/src/main/java/com/uom/icar/MainActivity.java
@@ -1,35 +1,39 @@
 package com.uom.icar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
-
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import com.google.android.material.navigation.NavigationView;
+import com.uom.icar.R;
+import com.uom.icar.SharedPreference;
 import com.uom.icar.databinding.ActivityMainBinding;
 import com.uom.icar.ui.home.HomeFragment;
 import com.uom.icar.ui.login.LoginFragment;
-import com.uom.icar.ui.manageService.AddServiceFragment;
-import com.uom.icar.ui.manageVehicle.AddVehicleFragment;
-import com.uom.icar.ui.manageVehicle.EditVehicleFragment;
 import com.uom.icar.ui.register.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    boolean status= false;
+    boolean register= false;
+    String userType;
+    String NIC;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +41,19 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        boolean status= false;
-        boolean register= false;
-        String userType;
-        String NIC;
 
         setSupportActionBar(binding.appBarMain.toolbar);
-
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home,R.id.nav_login,R.id.nav_register)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-//        NavigationUI.setupWithNavController(navigationView, navController);
+        //NavigationUI.setupWithNavController(navigationView, navController);
 
         //adding the navigation manually
         getSupportFragmentManager().popBackStack();
@@ -66,28 +65,19 @@ public class MainActivity extends AppCompatActivity {
         status = preference.GetBoolean(getApplicationContext(),SharedPreference.LOGIN_STATUS);
         NIC=preference.GetString(getApplicationContext(),SharedPreference.USER_NIC);
 
+        NIC=preference.GetString(getApplicationContext(),SharedPreference.USER_NIC);
+
 
         Temp.setNIC(NIC);
 
-//        Menu menu1 = navigationView.getMenu();
-//        MenuItem item1=menu1.findItem(R.id.nav_home);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_gallery);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_slideshow);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_addVehicle);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_addService);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_editVehicle);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_register);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_logout);
-//        item1.setVisible(false);
-//        item1=menu1.findItem(R.id.nav_exit);
-//        item1.setVisible(false);
+
+        Menu menu1 = navigationView.getMenu();
+        MenuItem item1=menu1.findItem(R.id.nav_home);
+        item1.setVisible(false);
+        item1=menu1.findItem(R.id.nav_logout);
+        item1.setVisible(false);
+        item1=menu1.findItem(R.id.nav_register);
+        item1.setVisible(false);
 
 
 
@@ -98,31 +88,20 @@ public class MainActivity extends AppCompatActivity {
 
             //check login
             if(status) {
-//                item=menu.findItem(R.id.nav_home);
-//                item.setVisible(true);
-//                item=menu.findItem(R.id.nav_gallery);
-//                item.setVisible(true);
-//                item=menu.findItem(R.id.nav_slideshow);
-//                item.setVisible(true);
-//                item=menu.findItem(R.id.nav_addService);
-//                item.setVisible(true);
-//                item=menu.findItem(R.id.nav_addVehicle);
-//                item.setVisible(true);
-//                item=menu.findItem(R.id.nav_logout);
-//                item.setVisible(true);
-//                item=menu.findItem(R.id.nav_exit);
-//                item.setVisible(true);
-//
-//
-//                item=menu.findItem(R.id.nav_login);
-//                item.setVisible(false);
-//
-//
-//                //moving to frag
-//                HomeFragment fragment = new HomeFragment();
-//                trans.replace(R.id.nav_host_fragment_content_main, fragment);
-//                trans.addToBackStack(null);
-//                trans.commit();
+                item1=menu1.findItem(R.id.nav_home);
+                item1.setVisible(true);
+                item1=menu1.findItem(R.id.nav_logout);
+                item1.setVisible(true);
+
+                item=menu.findItem(R.id.nav_login);
+                item.setVisible(false);
+
+
+                //moving to frag
+                HomeFragment fragment = new HomeFragment();
+                trans.replace(R.id.nav_host_fragment_content_main, fragment);
+                trans.addToBackStack(null);
+                trans.commit();
             }
             else {
                 //moving to frag
@@ -146,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -153,30 +133,19 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().popBackStack();
                 FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
 
-                if(menuID==R.id.nav_addVehicle){
-                    AddVehicleFragment fragment =new AddVehicleFragment();
+                if(menuID==R.id.nav_register){
+                    RegisterFragment fragment =new RegisterFragment();
                     trans.replace(R.id.nav_host_fragment_content_main,fragment);
                 }
-                else if(menuID ==R.id.nav_editVehicle){
-                    EditVehicleFragment fragment =new EditVehicleFragment();
-                    trans.replace(R.id.nav_host_fragment_content_main,fragment);
-                }
-//                else if(menuID ==R.id.nav_home){
-//                    HomeFragment fragment =new HomeFragment();
-//                    trans.replace(R.id.nav_host_fragment_content_main,fragment);
-//                }
                 else if(menuID ==R.id.nav_login){
                     LoginFragment fragment =new LoginFragment();
                     trans.replace(R.id.nav_host_fragment_content_main,fragment);
                 }
-                else if(menuID ==R.id.nav_register){
-                    RegisterFragment fragment =new RegisterFragment();
+                else if(menuID ==R.id.nav_home){
+                    HomeFragment fragment =new HomeFragment();
                     trans.replace(R.id.nav_host_fragment_content_main,fragment);
                 }
-                else if(menuID ==R.id.nav_addService){
-                    AddServiceFragment fragment =new AddServiceFragment();
-                    trans.replace(R.id.nav_host_fragment_content_main,fragment);
-                }
+
                 else if (menuID==R.id.nav_exit){
                     finish();
 
@@ -186,23 +155,14 @@ public class MainActivity extends AppCompatActivity {
                     trans.replace(R.id.nav_host_fragment_content_main,fragment);
                     trans.addToBackStack(null);
 
-                    preference.SaveBool(getApplicationContext(),false,SharedPreference.LOGIN_STATUS);
+                    preference.SaveBool(getApplicationContext(),false, SharedPreference.LOGIN_STATUS);
                     preference.SaveString(getApplicationContext(),null,SharedPreference.USER_NIC);
 
                     Menu menu1 = navigationView.getMenu();
                     MenuItem item1=menu1.findItem(R.id.nav_home);
                     item1.setVisible(false);
-                    item1=menu1.findItem(R.id.nav_addVehicle);
-                    item1.setVisible(false);
-                    item1=menu1.findItem(R.id.nav_addVehicle);
-                    item1.setVisible(false);
-                    item1=menu1.findItem(R.id.nav_addService);
-                    item1.setVisible(false);
                     item1=menu1.findItem(R.id.nav_logout);
                     item1.setVisible(false);
-                    item1=menu1.findItem(R.id.nav_register);
-                    item1.setVisible(false);
-
 
                 }
                 trans.addToBackStack(null);
@@ -226,5 +186,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 
 }
